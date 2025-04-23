6ae781ed1771db9120894e21562e064adc1021b5
Brian Engel <clutch_42@yahoo.com>
2025-04-23 18:18:24 -0500

    added hiking page

--- src/HobbyDetails.tsx
+++ src/HobbyDetails.tsx
@@ -5,7 +5,12 @@ import './HobbyDetails.css';
 const hobbyData: Record<string, { title: string; description: string; image: string }> = {
   'hiking': {
     title: 'Hiking',
-    description: 'placeholder for hiking description',
+    description: 'While most people prefer to spend their vacations relaxing on a beach, I\'ve always been drawn to national parks. There\'s \
+    something about being surrounded by nature—hiking through quiet trails, spotting wildlife, and moving through wide, open landscapes—that \
+    makes me feel more at peace than any beach ever could. But it\'s more than just scenery. National parks are wild. They\'re unpredictable. \
+    People get lost, weather turns fast, and yes, people die out there. And maybe that\'s part of why I go. It\'s real. Reaching a peak or following \
+    a snow-covered trail means something because it\'s not guaranteed. I\'m always trying to push myself—go a little farther, climb a little higher—not \
+    to prove anything, but because that edge, that risk, makes everything feel more alive. It\'s a chance to disconnect and reset in a way that comfort never could.',
     image: '/images/teddy_dinosaur.jpg',
   },
   'darts': {
@@ -20,12 +25,12 @@ const hobbyData: Record<string, { title: string; description: string; image: str
   },
   'bartending': {
     title: 'bartending',
-    description: 'placeholder for gaming description',
+    description: 'placeholder for bartending description',
     image: '/images/bartending.jpg'
   },
   'baseball': {
     title: 'baseball',
-    description: 'placeholder for gaming description',
+    description: 'placeholder for baseball description',
     image: '/images/baseball.jpg'
   },
   'cooking': {

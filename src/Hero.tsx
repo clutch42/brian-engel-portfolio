a9967d123b3671633d7b912f99b129b0aff99811
Brian Engel <clutch_42@yahoo.com>
2025-04-26 09:12:15 -0500

    fixed hiking image and got rid of lazy loading

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -23,45 +23,43 @@ const Hero: React.FC = () => {
       </div>
       <div className="hero-hobbies-container">
         <h2 className="hero-hobbies-title"></h2>
-        
-          <ul className="hero-hobbies-list">
-            <li className="hobby-card">
-              <Link to="hobby/bartending">
-                <img src="/images/bartending.webp" alt="bartending" className="hobby-image" loading="lazy" />
-              </Link>
-            </li>
-            <li className="hobby-card">
-              <Link to="hobby/hiking">
-                <img src="/images/teddy_dinosaur.webp" alt="teddy and dinosaur" className="hobby-image" loading="lazy" />
-              </Link>
-            </li>
-            <li className="hobby-card">
-              <Link to="hobby/darts">
-                <img src="/images/darts.webp" alt="darts" className="hobby-image" loading="lazy"  />
-              </Link>
-            </li>
-            <li className="hobby-card">
-              <Link to="hobby/gaming">
-                <img src="/images/gaming.webp" alt="gaming" className="hobby-image" loading="lazy" />
-              </Link>
-            </li>
-            <li className="hobby-card">
-              <Link to="hobby/baseball">
-                <img src="/images/baseball.webp" alt="baseball" className="hobby-image" loading="lazy" />
-              </Link>
-            </li>
-            <li className="hobby-card">
-              <Link to="hobby/cooking">
-                <img src="/images/cooking.webp" alt="cooking" className="hobby-image" loading="lazy" />
-              </Link>
-            </li>
-            <li className="hobby-card">
-              <Link to="hobby/disc_golf">
-                <img src="/images/disc_golf.webp" alt="disc golf" className="hobby-image" loading="lazy" />
-              </Link>
-            </li>
-
-          </ul>
+        <ul className="hero-hobbies-list">
+          <li className="hobby-card">
+            <Link to="hobby/bartending">
+              <img src="/images/bartending.webp" alt="bartending" className="hobby-image"/>
+            </Link>
+          </li>
+          <li className="hobby-card">
+            <Link to="hobby/hiking">
+              <img src="/images/teddy_dinosaur.webp" alt="teddy and dinosaur" className="hobby-image hobby-image-top"/>
+            </Link>
+          </li>
+          <li className="hobby-card">
+            <Link to="hobby/darts">
+              <img src="/images/darts.webp" alt="darts" className="hobby-image"/>
+            </Link>
+          </li>
+          <li className="hobby-card">
+            <Link to="hobby/gaming">
+              <img src="/images/gaming.webp" alt="gaming" className="hobby-image"/>
+            </Link>
+          </li>
+          <li className="hobby-card">
+            <Link to="hobby/baseball">
+              <img src="/images/baseball.webp" alt="baseball" className="hobby-image"/>
+            </Link>
+          </li>
+          <li className="hobby-card">
+            <Link to="hobby/cooking">
+              <img src="/images/cooking.webp" alt="cooking" className="hobby-image"/>
+            </Link>
+          </li>
+          <li className="hobby-card">
+            <Link to="hobby/disc_golf">
+              <img src="/images/disc_golf.webp" alt="disc golf" className="hobby-image"/>
+            </Link>
+          </li>
+        </ul>
         
       </div>
     </div>

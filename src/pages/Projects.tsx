cb92fa87c8c7a5b9df705e3037d3dd3aa4f38e27
Brian Engel <clutch_42@yahoo.com>
2025-08-07 13:36:00 -0500

    save

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     <section className="projects-container">
       <div className="projects-section">
         <h2>Projects</h2>
-        <p>Here's a collection of work showcasing what I’ve built and explored.</p>
+        <p>Here's a collection of work showcasing what I've built and explored.</p>
         {/* Projects will go here */}
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
